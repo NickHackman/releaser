d812c46f706a73d1d0ffd626cabf9d305df2992e
Nick Hackman <snickhackman@gmail.com>
2021-12-15 21:19:19 -0500

    refactor: create colors package

---
+++ ui/colors/colors.go
@@ -0,0 +1,11 @@
+package colors
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	Selected      = lipgloss.Color("#bf94e4")
+	Title         = lipgloss.Color("#967bb6")
+	Url           = lipgloss.Color("#7c5295")
+	ProgressStart = "#967bb6"
+	ProgressEnd   = "#bf94e4"
+)
