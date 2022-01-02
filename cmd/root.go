1d2d1c0db7bc0e81763e43389a4abe99e4835da8
Nick Hackman <snickhackman@gmail.com>
2022-01-02 11:04:26 -0500

    refactor!: remove publish cmd in favor of root
    Publish was an extra thing to type that didn't provide any value since it'll be the only real reason you'd want to run this application. Removed it in favor of using the root command.

--- cmd/root.go
+++ cmd/root.go
@@ -27,7 +27,12 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
 
+	"github.com/NickHackman/releaser/internal/config"
+	"github.com/NickHackman/releaser/internal/github"
+	"github.com/NickHackman/releaser/internal/tui"
+	"github.com/NickHackman/releaser/internal/version"
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -45,22 +50,82 @@ var rootCmd = &cobra.Command{
 	Long: `A CLI application that will take the diff of all repositories 
 in a GitHub organization and create releases for only the repositories
 that have changes. The description associated with the release is
-a Go Sprig template that will be used by default for all descriptions.
+a Go Sprig template that will be used for all descriptions.
 
-GitHub Oauth:
+Version Methods:
 
-releaser login
+Method to determine the new version.
 
-releaser --url "git.enterprise.com" login
+Methods:
+major        Increment Major version 1.3.0 -> 2.0.0
+minor        Increment Minor version 1.3.0 -> 1.4.0
+patch        Increment Patch version 1.3.0 -> 1.3.1
+
+Template:
+
+Top Level Variables:
+
+{{ .RepositoryName }}                  hello-world	
+{{ .RepositoryOwner }}                 octocat
+{{ .RepositoryURL }}                   https://github.com/octocat/hello-world	
+{{ .RepositoryDescription }}           Example description
+{{ .RepositoryDefaultBranch }}         main
+{{ .Commits }}                         List of commits
+
+Commit:
+
+{{ .Sha }}                             Unique identifier for commit
+{{ .URL }}                             URL to commit
+{{ .Summary }}                         First line of the commit message
+{{ .Message }}                         Full commit message (includes newlines)
+
+Author/Committer:
+{{ .AuthorUsername }}                  octocat (GitHub Username)
+{{ .AuthorName }}                      octocat (Commit Name)
+{{ .AuthorEmail }}                     octocat@github.com
+{{ .AuthorDate }} 
+{{ .AuthorURL }}                       https://github.com/octocat
+
+Templates also include Sprig functions: https://masterminds.github.io/sprig/strings.html
+
+Template Example:
+
+{{ range .Commits }}
+{{ substr 0 8 .Sha }} committed by {{ .CommitterUsername }} and authored by {{ .AuthorUsername }} {{ .Summary }}
+{{ end }}
 
 Examples:
 
-releaser publish --org "GitHub org"
+releaser login
 
-releaser --config "releaser.yml" publish --org "GitHub org"
+releaser login --auth.url git.enterprise.com
 
-releaser publish --org "GitHub org" --template "This is an example"`,
-	Run: func(cmd *cobra.Command, args []string) {},
+releaser --org example --version.change minor
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		token := viper.GetString("token")
+		url := viper.GetString("url")
+
+		gh, err := github.New().URL(url).Token(token).Build()
+		if err != nil {
+			return err
+		}
+
+		change, err := version.ChangeFromString(viper.GetString("version.change"))
+		if err != nil {
+			return err
+		}
+
+		config := &config.Config{
+			Branch:         viper.GetString("branch"),
+			Org:            viper.GetString("org"),
+			Timeout:        viper.GetDuration("timeout"),
+			TemplateString: viper.GetString("template"),
+			VersionChange:  change,
+		}
+
+		return tui.Execute(gh, config)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -73,11 +138,21 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $XDG_CONFIG_HOME/releaser.yaml)")
-	rootCmd.PersistentFlags().String("url", "https://api.github.com/", "GitHub API url")
-	rootCmd.PersistentFlags().String("token", "", "GitHub Oauth Token")
+	rootCmd.Flags().String("url", "https://api.github.com/", "GitHub API url")
+	rootCmd.Flags().String("token", "", "GitHub Oauth Token")
+	rootCmd.Flags().StringP("org", "o", "", "GitHub organization to create releases")
+	rootCmd.Flags().String("template", "", "Go template that is the default message for all releases")
+	rootCmd.Flags().DurationP("timeout", "t", time.Minute, "Timeout duration to wait for GitHub to respond before exiting")
+	rootCmd.Flags().StringP("branch", "b", "", "Branch to create releases on (defaults to Repository's default branch)")
+	rootCmd.Flags().String("version.change", "", "Method to determine the new version based off the previous (major, minor, patch)")
 
-	cobra.CheckErr(viper.BindPFlag("token", rootCmd.PersistentFlags().Lookup("token")))
-	cobra.CheckErr(viper.BindPFlag("url", rootCmd.PersistentFlags().Lookup("url")))
+	cobra.CheckErr(viper.BindPFlag("template", rootCmd.Flags().Lookup("template")))
+	cobra.CheckErr(viper.BindPFlag("org", rootCmd.Flags().Lookup("org")))
+	cobra.CheckErr(viper.BindPFlag("timeout", rootCmd.Flags().Lookup("timeout")))
+	cobra.CheckErr(viper.BindPFlag("branch", rootCmd.Flags().Lookup("branch")))
+	cobra.CheckErr(viper.BindPFlag("version.change", rootCmd.Flags().Lookup("version.change")))
+	cobra.CheckErr(viper.BindPFlag("token", rootCmd.Flags().Lookup("token")))
+	cobra.CheckErr(viper.BindPFlag("url", rootCmd.Flags().Lookup("url")))
 }
 
 // initConfig reads in config file and ENV variables if set.
